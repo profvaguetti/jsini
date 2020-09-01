b23020df68ea2e410c6fd060888bad9015480dc7
Prof. Leandro Vaguetti <prof.vaguetti@gmail.com>
2020-09-01 10:47:26 +0000

    Indrodução IF (condicional)

--- modules/objetos/OperArit.js
+++ modules/objetos/OperArit.js
@@ -24,3 +24,17 @@ class OperArit{
 
 }
 module.exports = OperArit;
+
+/*
+const OperArit = require('./modules/objetos/OperArit.js');
+
+var calculadora = new OperArit(10,5); // esquerda e direita do operador
+console.log(calculadora.soma());
+console.log(calculadora.subtracao());
+console.log(calculadora.multiplicacao());
+console.log(calculadora.divisao());
+console.log(calculadora.mod());
+console.log(calculadora.potencia());
+
+
+*/
